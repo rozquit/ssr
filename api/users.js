27f53aa027c991bc1b4cd48977f1f128c3d896da
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-09-17 22:41:10 +0300

    WIP

--- api/users.js
+++ api/users.js
@@ -1,17 +1,42 @@
 'use strict';
 
-module.exports
-	= async () => [
-		{
-			'name': 'Mr. Arch',
-			'role': 'admin',
-			'email': 'arttolstykh@gmail.com',
-			'isActive': true
-		},
-		{
-			'name': 'Guest',
-			'role': 'guest',
-			'email': 'guest@example.com',
-			'isActive': false
-		}
-	];
+const querystring = require('querystring');
+
+// http://127.0.0.1:8080/users?role=admin&status=active
+const search
+	= query => {
+		const queryObject = querystring.parse(query.substring(1));
+		console.log('[api | users | search]', [queryObject]);
+		return [
+			{
+				'name': 'Mr. Arch',
+				'role': 'admin',
+				'email': 'arttolstykh@gmail.com',
+				'isActive': true
+			}
+		];
+	};
+
+// http://127.0.0.1:8080/users
+const users = async ({location}) => {
+	console.log('[api | users]', [location]);
+	const query = location.search;
+	return query
+		? search(query)
+		: [
+			{
+				'name': 'Mr. Arch',
+				'role': 'admin',
+				'email': 'arttolstykh@gmail.com',
+				'isActive': true
+			},
+			{
+				'name': 'Guest',
+				'role': 'guest',
+				'email': 'guest@example.com',
+				'isActive': false
+			}
+		];
+}
+
+module.exports = users;

79b4ac766b50ed1d6886099ccfa1ef6d65dc305c
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-09-23 20:34:36 +0300

    WIP

--- arch.js
+++ arch.js
@@ -23,88 +23,6 @@ const MIME_TYPES = {
 
 const api = new Map()
 
-const types = {
-  object: ([data], callback) => callback(JSON.stringify(data)),
-  undefined: (args, callback) => callback('Not Found'),
-  function: ([fn, req, res], callback) => {
-    if (fn.length === 3) fn(req, res, callback)
-    else callback(JSON.stringify(fn(req, res)))
-  }
-}
-
-const serve =
-	(data, req, res) => {
-	  const type = typeof data
-	  if (type === 'string') return res.end(data)
-	  const serializer = types[type]
-	  serializer([data, req, res], data => serve(data, req, res))
-	}
-
-const serveFile =
-	name => {
-	  const filePath = path.join(STATIC_PATH, name)
-	  if (!filePath.startsWith(STATIC_PATH)) return null
-	  return fs.createReadStream(filePath)
-	}
-
-const receiveArgs =
-	async req =>
-	  new Promise((resolve, reject) => {
-	    const body = []
-	    req
-	      .on('data', chunk => {
-	        body.push(chunk)
-	      })
-	      .on('end', async () => {
-	        const data = body.join(',')
-	        data
-	          ? resolve(JSON.parse(data))
-	          : reject('Bad Request')
-	      })
-	  })
-
-const cacheFile =
-	name => {
-	  const filePath = `${API_PATH}/${name}`
-	  const key = path.basename(filePath, '.js')
-	  try {
-	    const libPath = require.resolve(filePath)
-	    delete require.cache[libPath]
-	  } catch (e) {
-	    return
-	  }
-	  try {
-	    const method = require(filePath)
-	    api.set(key, method)
-	  } catch (e) {
-	    api.delete(name)
-	  }
-	}
-
-const cacheFolder =
-	path => {
-	  fs.readdir(path, (err, files) => {
-	    if (err) return
-	    files.forEach(cacheFile)
-	  })
-	}
-
-const watch =
-	path => {
-	  fs.watch(path, (event, file) => {
-	    cacheFile(file)
-	  })
-	}
-
-cacheFolder(API_PATH)
-watch(API_PATH)
-
-const httpError =
-	(res, status, message) => {
-	  res.statusCode = status
-	  res.end(`${message}`)
-	}
-
 function arch (options) {
   options = options || {}
 
@@ -119,6 +37,88 @@ function arch (options) {
   const { logger } = createLogger(options)
 
   if (pages.length) prerender(options)
+
+  const types = {
+    object: ([data], callback) => callback(JSON.stringify(data)),
+    undefined: (args, callback) => callback('Not Found'),
+    function: ([fn, req, res], callback) => {
+      if (fn.length === 3) fn(req, res, callback)
+      else callback(JSON.stringify(fn(req, res)))
+    }
+  }
+
+  const serve =
+    (data, req, res) => {
+      const type = typeof data
+      if (type === 'string') return res.end(data)
+      const serializer = types[type]
+      serializer([data, req, res], data => serve(data, req, res))
+    }
+
+  const serveFile =
+    name => {
+      const filePath = path.join(STATIC_PATH, name)
+      if (!filePath.startsWith(STATIC_PATH)) return null
+      return fs.createReadStream(filePath)
+    }
+
+  const receiveArgs =
+    async req =>
+      new Promise((resolve, reject) => {
+        const body = []
+        req
+          .on('data', chunk => {
+            body.push(chunk)
+          })
+          .on('end', async () => {
+            const data = body.join(',')
+            data
+              ? resolve(JSON.parse(data))
+              : reject('Bad Request')
+          })
+      })
+
+  const cacheFile =
+    name => {
+      const filePath = `${API_PATH}/${name}`
+      const key = path.basename(filePath, '.js')
+      try {
+        const libPath = require.resolve(filePath)
+        delete require.cache[libPath]
+      } catch (e) {
+        return
+      }
+      try {
+        const method = require(filePath)
+        api.set(key, method)
+      } catch (e) {
+        api.delete(name)
+      }
+    }
+
+  const cacheFolder =
+    path => {
+      fs.readdir(path, (err, files) => {
+        if (err) return
+        files.forEach(cacheFile)
+      })
+    }
+
+  const watch =
+    path => {
+      fs.watch(path, (event, file) => {
+        cacheFile(file)
+      })
+    }
+
+  cacheFolder(API_PATH)
+  watch(API_PATH)
+
+  const httpError =
+    (res, status, message) => {
+      res.statusCode = status
+      res.end(`${message}`)
+    }
 
   const routeHandler =
 		(req, res, callback) => {

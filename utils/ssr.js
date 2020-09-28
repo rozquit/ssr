814d0c836bd9c26fd9435e7c6aa3beae2905e008
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-09-28 12:47:35 +0300

    WIP

--- utils/ssr.js
+++ utils/ssr.js
@@ -17,41 +17,31 @@ const ssr = async (host, pathname, query) => {
     const browser = await puppeteer.launch()
     browserWSEndpoint = await browser.wsEndpoint()
   }
-
   const url = `http://${host}/${pathname.substring(1) ? `${pathname.substring(1)}/index.html` : 'index.html'}${
     query ? `${query}` : ''
   }`
-
   if (RENDER_CACHE.has(url)) {
     return { html: RENDER_CACHE.get(url), ttRenderMs: 0 }
   }
-
   const start = Date.now()
   const browser = await puppeteer.connect({ browserWSEndpoint })
   const page = await browser.newPage()
   await page.setRequestInterception(true)
-
   page.on('request', req => {
     const allowList = ['document', 'script', 'xhr', 'fetch']
     if (!allowList.includes(req.resourceType())) return req.abort()
     req.continue()
   })
-
   try {
     await page.goto(url, { waitUntil: 'networkidle0' })
     await page.waitForSelector('#container')
   } catch (err) {
     throw new Error('page.goto/waitForSelector timed out.')
   }
-
   const html = await page.content()
-
   await page.close()
-
   const ttRenderMs = Date.now() - start
-
   RENDER_CACHE.set(url, html)
-
   return { html, ttRenderMs }
 }
 
